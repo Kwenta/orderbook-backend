cb1fa361f05d2fc1e3f074c6a770848fe2eabacd
Daniel <16984675+DanielRX@users.noreply.github.com>
2024-10-03 16:18:43 +0100

    Add documentation to the schemas

--- packages/orderbook/src/schemas/solidity.ts
+++ packages/orderbook/src/schemas/solidity.ts
@@ -13,57 +13,65 @@ import type {
 	uint,
 } from '../types'
 
-export const hexString = (n = 1000, strict = false) =>
-	z
+export const hexString = (n = 1000, strict = false, description?: string) => {
+	const schema = z
 		.string()
 		.refine((s) => (s.startsWith('0x') && strict ? s.length === n * 2 + 2 : s.length <= n * 2 + 2))
 		.transform((s) => s as HexString)
 
-export const address = () =>
-	hexString()
+	return description ? schema.describe(description) : schema
+}
+export const address = (description?: string) => {
+	return hexString(20, true, description ?? 'A valid Ethereum address')
 		.refine((s) => isAddress(s))
 		.transform((s) => checksumAddress(s))
-
-const bytesSchema = (n: BytesSizes = 32): ZodBytes<keyof bytes> => {
-	if (n < 0 || n > 32) throw new Error('Invalid bytes size')
-	return hexString(n, true) as ZodBytes<keyof bytes>
 }
 
-const uintSchema = (n: Sizes = 256): ZodUint<keyof uint> => {
+const bytesSchema = (n: BytesSizes = 32, description?: string): ZodBytes<keyof bytes> => {
+	if (n < 0 || n > 32) throw new Error('Invalid bytes size')
+
+	return hexString(n, true, description) as ZodBytes<keyof bytes>
+}
+
+const uintSchema = (n: Sizes = 256, description?: string): ZodUint<keyof uint> => {
 	if (n < 0 || n > 256) throw new Error('Invalid uint size')
 	const maxValue = viem[`maxUint${n}`]
-	return z
+	const schema = z
 		.string()
 		.transform(BigInt)
 		.refine(
 			(x) => x >= BigInt(0) && x < maxValue,
 			`Value must be between 0 and ${maxValue}`
 		) as ZodUint<keyof uint>
+
+	return description ? schema.describe(description) : schema
 }
 
-const intSchema = (n: Sizes = 256): ZodInt<keyof int> => {
+const intSchema = (n: Sizes = 256, description?: string): ZodInt<keyof int> => {
 	if (n < 0 || n > 256) throw new Error('Invalid uint size')
 	const maxValue = viem[`maxInt${n}`]
 	const minValue = viem[`minInt${n}`]
-	return z
+	const schema = z
 		.string()
 		.transform(BigInt)
 		.refine(
 			(x) => x > minValue && x < maxValue,
 			`Value must be between ${minValue} and ${maxValue}`
 		) as ZodInt<keyof int>
+
+	return description ? schema.describe(description) : schema
 }
 
-export const bytes4 = () => bytesSchema(4) as ZodBytes<4>
-export const bytes32 = () => bytesSchema(32) as ZodBytes<32>
+export const bytes4 = (description?: string) => bytesSchema(4, description) as ZodBytes<4>
+export const bytes32 = (description?: string) => bytesSchema(32, description) as ZodBytes<32>
 
-export const uint8 = () => uintSchema(8) as ZodUint<8>
-export const uint128 = () => uintSchema(128) as ZodUint<128>
-export const uint256 = () => uintSchema(256) as ZodUint<256>
+export const uint8 = (description?: string) => uintSchema(8, description) as ZodUint<8>
+export const uint128 = (description?: string) => uintSchema(128, description) as ZodUint<128>
+export const uint256 = (description?: string) => uintSchema(256, description) as ZodUint<256>
 
-export const int8 = () => intSchema(8) as ZodInt<8>
-export const int128 = () => intSchema(128) as ZodInt<128>
-export const int256 = () => intSchema(256) as ZodInt<256>
+export const int8 = (description?: string) => intSchema(8, description) as ZodInt<8>
+export const int128 = (description?: string) => intSchema(128, description) as ZodInt<128>
+export const int256 = (description?: string) => intSchema(256, description) as ZodInt<256>
 
-export const FunctionSelector = bytes4()
-export const Signature = bytes32()
+export const FunctionSelector = (description?: string) => bytes4(description)
+export const Signature = (description?: string) => bytes32(description)
