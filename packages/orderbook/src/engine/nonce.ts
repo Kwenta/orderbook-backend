182b39763735e0ae898cbb358b7c1a7c567d8fe0
Daniel <16984675+DanielRX@users.noreply.github.com>
2024-10-03 14:17:07 +0100

    Use accountId for nonces

--- packages/orderbook/src/engine/nonce.ts
+++ packages/orderbook/src/engine/nonce.ts
@@ -1,20 +1,19 @@
 import { uint256 } from 'schemas/solidity'
-import type { Address } from 'viem'
-import type { uint } from '../types'
+import type { AccountId, uint } from '../types'
 
-const nonces = new Map<Address, { nonce: uint['256']; lastSeen: Date }>()
+const nonces = new Map<AccountId, { nonce: uint['256']; lastSeen: Date }>()
 
-export const nonceOfUser = (user: Address) => {
+export const nonceOfUser = (user: AccountId) => {
 	return nonces.get(user) ?? { nonce: 0n, lastSeen: new Date() }
 }
 
-export const getAndIncrementNonce = (user: Address) => {
+export const getAndIncrementNonce = (user: AccountId) => {
 	const nonce = nonceOfUser(user)
 	nonces.set(user, { nonce: uint256().parse(nonce.nonce + 1n), lastSeen: new Date() })
 	return nonce
 }
 
-export const incrementNonce = (user: Address) => {
+export const incrementNonce = (user: AccountId) => {
 	const nonce = nonceOfUser(user)
 	nonces.set(user, { nonce: uint256().parse(nonce.nonce + 1n), lastSeen: nonce.lastSeen })
 }
