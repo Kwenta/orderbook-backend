ab8fd4981076b7bbe80b10965b6df5f2ece6fc29
Daniel <16984675+DanielRX@users.noreply.github.com>
2024-10-10 13:38:28 +0100

    Add logger

---
+++ packages/orderbook/src/logger.ts
@@ -0,0 +1,29 @@
+import * as path from 'path'
+import { Logger } from 'tslog'
+import { LOG_COLOURS, LOG_LEVELS } from './constants'
+import { perfFunc } from './monitoring'
+
+export const ansiColorWrap = perfFunc('ansiColorWrap')(
+	(val: string | number, code: keyof typeof LOG_COLOURS) =>
+		`\u001b[${LOG_COLOURS[code]}m${val}\u001b[0m`
+)
+
+const root = `${path.join(__dirname, '..').replace(/^[a-zA-Z]:/, '')}\\`
+
+export const logger = new Logger({
+	name: 'orderbook       ',
+	overwrite: {
+		formatMeta: (meta) => {
+			if (!meta) return ''
+			const { date, logLevelName, path, name } = meta
+			const location = (
+				path?.fullFilePath ? ` ${path?.fileName}:${path?.fileLine}:${path?.fileColumn}` : ''
+			).replace(root, '')
+
+			const level = logLevelName as keyof typeof LOG_LEVELS
+
+			const colour = LOG_LEVELS[level] ?? LOG_LEVELS.INFO
+			return `${date.toISOString()} {${name}} ${`[${ansiColorWrap(logLevelName, colour)}]`.padEnd(20, ' ')} ${location.padEnd(50, ' ')} `
+		},
+	},
+})
